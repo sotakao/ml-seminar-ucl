b6aed96a72e219a5b3b07c17cc058c1297254ba8
So Takao <sotakao@eduroam-int-dhcp-97-94-224.ucl.ac.uk>
2023-01-16 17:32:57 +0000

    updated markdown texts in cora example

--- factorgraph.py
+++ factorgraph.py
@@ -1,7 +1,7 @@
 import numpy as np
 import networkx as nx
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import Dict, List, Tuple, Union
 
 
 class FactorGraph:
@@ -11,7 +11,7 @@ class FactorGraph:
         ----------
         bipartitegraph: A networkx Graph representing the structure of the factor graph. This should be a bipartite graph
                         (see https://networkx.org/documentation/stable/reference/algorithms/bipartite.html)
-                        whith edges represented as (variable, factor).
+                        whith edges e represented as e = (variable, factor).
         variable_nodes: A dictionary whose keys are variable names (as used in bipartitegraph) and values are VariableNode objects.
         factor_nodes: A dictionary whose keys are factor node labels (as used in bipartitegraph) and values are FactorNode objects.
         """
@@ -19,14 +19,16 @@ class FactorGraph:
 
         for edge in bipartitegraph.edges:
             var_key, fac_key = edge
-            assert var_key in variable_nodes.keys(), f"variable {var_key} not found in variable_nodes dictionary"
-            assert fac_key in factor_nodes.keys(), f"factor {fac_key} not found in factor_nodes dictionary"
+            assert var_key in variable_nodes.keys(), f"variable {var_key} not found in variable_nodes"
+            assert fac_key in factor_nodes.keys(), f"factor {fac_key} not found in factor_nodes"
 
-        for var_key in variable_nodes.keys():
+        for var_key, variable in variable_nodes.items():
             assert var_key in bipartitegraph.nodes, f"variable {var_key} does not correspond to a node in bipartitegraph"
+            assert isinstance(variable, VariableNode), f"variable {var_key} must be a VariableNode object"
 
-        for fac_key in factor_nodes.keys():
-            assert fac_key in bipartitegraph.nodes, f"factor {var_key} does not correspond to a node in bipartitegraph"
+        for fac_key, factor in factor_nodes.items():
+            assert fac_key in bipartitegraph.nodes, f"factor {fac_key} does not correspond to a node in bipartitegraph"
+            assert isinstance(factor, FactorNode), f"factor {fac_key} must be a FactorNode object"
 
         self.graph = bipartitegraph
         self.variables = variable_nodes
@@ -38,18 +40,19 @@ class FactorGraph:
             varID, facID = edge
             variable = self.variables[varID]
             factor = self.factors[facID]
+            # Connect variable and factor nodes by an Edge object
             edge = Edge(num_states=variable.num_states)
-            variable.edges[facID] = edge
-            factor.edges[varID] = edge
+            variable.edges[facID] = edge # Connect variable to factor <facID> by an edge
+            factor.edges[varID] = edge # Connect factor to variable <varID> by an edge
 
     def reset(self):
         for variable in self.variables.values():
-            variable.reset()
+            variable.reset() # Reset messages and states to 1
 
 
 class Edge:
     """
-    Edge object contain messages sent between factor/varaiable nodes in a factor graph.
+    Edge object contains messages that are sent between factor/varaiable nodes in a factor graph.
     """
     def __init__(self, num_states):
         self.num_states = num_states # Number of possible states a variable can take
@@ -71,30 +74,47 @@ class Node(ABC):
 
     @property
     def edges(self):
-        for edge in self._edges.values():
-            assert isinstance(edge, Edge)
+        # edges must be a dictionary whose values are Edge objects
+        for edge_key, edge in self._edges.items():
+            assert isinstance(edge, Edge), f"edge {edge_key} must be an Edge object"
         return self._edges
 
     @abstractmethod
     def send_message(self, outedgeID):
         """
-        Abstract method to send message to a neighbouring node 'outedegID'
+        Abstract method to send a message to a neighbouring node <outedgeID>
         """
         pass
 
     def send_all_messages(self):
         """
-        Send message to all neighbours
+        Send messages to all neighbours
         """
         for outedgeID in self.edges.keys():
             self.send_message(outedgeID)
 
 
 class Potential:
-    """
-    The potential function consists of two objects
-    """
-    def __init__(self, tensor, variables):
+    def __init__(self, tensor: np.ndarray, variables: Union[List, Tuple]):
+        """
+        This class represents potential functions, arising in the decomposition of joint distributions in Markov random fields.
+        (Can also be used to model conditional distributions in Bayesian networks)
+        We only consider the discrete variable setting here and not the continuous setting.
+        In this case, a potential function can be characterised by:
+        (1) a tensor, that stores the values of the potential function in tensorial form, and
+        (2) an ordered list/tuple of variable names, indicating which variable corresponds to which axis of the tensor.
+        
+        Example:
+        ----------
+        Consider a potential function in two variables a and b, with K discrete states X = {1, ..., K}.
+        Setting
+        - tensor = P, and
+        - variables = ['a', 'b']
+        for some K x K tensor P, yields a potential function f : X x X -> R, such that f(a=i, b=j) = P[i,j] for i,j = 1, ..., K.
+
+        Note: In general, when the potential is a function of N variables, each with K discrete states, then P will be a K x ... x K (N times) tensor.
+
+        """
         assert tensor.ndim == len(variables), f"Dimensions of tensor ({tensor.ndim}) must match the number of variables {len(variables)}."
         self.tensor = tensor
         axes = np.arange(len(variables))
@@ -112,16 +132,20 @@ class VariableNode(Node):
 
     def send_message(self, outedgeID):
         """
-        Compute variable-to-factor message sent to a factor node with label 'outedgeID'
+        Compute variable-to-factor message sent to a factor node with label <outedgeID>
+        by multiplying all incoming factor-to-variable messages, except for that coming from factor <outedgeID>.
         """
         variabletofactormessage = np.ones(self.num_states)
         for edgeID, edge in self.edges.items():
             if (edgeID != outedgeID):
                 variabletofactormessage *= edge.factortovariablemessage
-        variabletofactormessage /= variabletofactormessage.sum() # Normalising message yields more stable results
+        variabletofactormessage /= variabletofactormessage.sum() # Normalising message lead to more stable results
         self.edges[outedgeID].variabletofactormessage = variabletofactormessage 
 
     def update(self):
+        """
+        Update state of the variable by multiplying together all incoming factor-to-variable messages
+        """
         state = np.ones(self.num_states)
         for edge in self.edges.values():
             state *= edge.factortovariablemessage
@@ -129,9 +153,9 @@ class VariableNode(Node):
         self.state = state
 
     def reset(self):
-        self.state = np.ones(self.num_states)
+        self.state = np.ones(self.num_states) # Reset state to a uniform distribution of ones
         for edge in self.edges.values():
-            edge.reset()
+            edge.reset() # Reset all messages to ones
 
 
 class FactorNode(Node):
@@ -140,9 +164,15 @@ class FactorNode(Node):
     """
     def __init__(self, potential: Potential):
         super().__init__()
+        assert isinstance(potential, Potential)
         self.potential = potential
 
     def send_message(self, outedgeID):
+        """
+        Compute factor-to-variable message sent to a variable node with label <outedgeID>
+        by filtering all incoming variable-to-factor messages (except for that coming from variable <outedgeID>)
+        through the potential function.
+        """
         M = self.potential.tensor
         for edgeID in self.edges.keys():
             if edgeID != outedgeID:
