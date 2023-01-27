8ed584f50f4d2655c89be3afebfcb279c2e02653
So Takao <sotakao@Sos-MacBook-Pro-2.local>
2023-01-27 01:16:29 +0000

    finished writing models and algorithms

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,8 @@
+"""
+@author: So Takao
+"""
 import numpy as np
-from factorgraph import TreeGraph, FactorGraph
+from models import TreeGraph, FactorGraph
 from copy import deepcopy
 
 
@@ -15,22 +18,9 @@ def _is_leaf(node, root_node, G: TreeGraph):
 
 
 def BeliefPropagation(tree: TreeGraph, root_node):
-    def update_message(source_node, target_node):
-        edge_potential = tree.edges[(target_node, source_node)]['potential'].tensor
-        node_potential = tree.nodes[source_node]['potential'].tensor
-        prod_msgs = deepcopy(node_potential)
-        for e in tree.in_edges(source_node):
-            if e != (target_node, source_node):
-                prod_msgs *= tree.edges[e]['message']
-        message = edge_potential @ prod_msgs
-        tree.edges[(source_node, target_node)]['message'] = message
-
-    def update_state(node):
-        state = np.ones(tree.num_states)
-        for e in tree.in_edges(node):
-            state *= tree.edges[e]['message']
-        state /= state.sum()
-        tree.nodes[node]['state'] = state
+    if tree.state_type == 'mode':
+        print('Switching state type to "marginal"...')
+        tree.state_type = 'marginal'
 
     # Step 1: Find leaf nodes corresponding to the root
     leaf_nodes = []
@@ -47,7 +37,7 @@ def BeliefPropagation(tree: TreeGraph, root_node):
             if _is_leaf(node, root_node, G):
                 # Update message
                 parent_node = list(G.neighbors(node))[0] # Note: a leaf has only one parent node
-                update_message(node, parent_node)
+                tree.send_message(node, parent_node)
                 next_nodes.append(parent_node)
                 G.remove_node(node)
             else:
@@ -62,35 +52,20 @@ def BeliefPropagation(tree: TreeGraph, root_node):
             # Update message
             offsprings = list(G.neighbors(node))
             for child_node in offsprings:
-                update_message(node, child_node)
+                tree.send_message(node, child_node)
                 next_nodes.append(child_node)
             G.remove_node(node)
         current_nodes = next_nodes
             
     # Update states
     for node in tree.nodes():
-        update_state(node)
+        tree.update_state(node)
 
 
 def MaxProduct(tree: TreeGraph, root_node):
-    def update_message(source_node, target_node):
-        edge_potential = tree.edges[(target_node, source_node)]['potential'].tensor
-        node_potential = tree.nodes[source_node]['potential'].tensor
-        prod_msgs = deepcopy(node_potential)
-        for e in tree.in_edges(source_node):
-            if e != (target_node, source_node):
-                prod_msgs *= tree.edges[e]['message']
-        message = np.max(edge_potential * prod_msgs[None], axis=1)
-        tree.edges[(source_node, target_node)]['message'] = message
-
-    def update_state(node, parent_node, offsprings):
-        x = tree.nodes[parent_node]['state']
-        edge_potential = tree.edges[(parent_node, node)]['potential'].tensor
-        node_potential = tree.nodes[node]['potential'].tensor
-        prod_msgs = deepcopy(node_potential)
-        for child_node in offsprings:
-            prod_msgs *= tree.edges[(child_node, node)]['message']
-        tree.nodes[node]['state'] = np.argmax(edge_potential[x,:] * prod_msgs)
+    if tree.state_type == 'marginal':
+        print('Switching state_type to "mode"...')
+        tree.state_type = 'mode'
 
     # Step 1: Find leaf nodes corresponding to the root
     leaf_nodes = []
@@ -106,7 +81,7 @@ def MaxProduct(tree: TreeGraph, root_node):
         for node in current_nodes:
             if _is_leaf(node, root_node, G):
                 parent_node = list(G.neighbors(node))[0] # Note: there is only one parent node
-                update_message(node, parent_node)
+                tree.send_message(node, parent_node)
                 G.remove_node(node)
                 next_nodes.append(parent_node)
             else:
@@ -128,7 +103,7 @@ def MaxProduct(tree: TreeGraph, root_node):
             G.remove_node(parent_node)
             for node in nodes:
                 offsprings = list(G.neighbors(node))
-                update_state(node, parent_node, offsprings)
+                tree.update_state(node, parent_node, offsprings)
                 next_nodes.append(node)
         current_nodes = next_nodes
 
@@ -145,6 +120,6 @@ def LoopyBP(factorgraph: FactorGraph, num_iter: int=1):
             factorgraph.send_all_messages(factor)
         # Update states
         for variable in factorgraph.variables:
-            factorgraph.update(variable)
+            factorgraph.update_state(variable)
 
 
---
+++ models.py
@@ -0,0 +1,253 @@
+"""
+@author: So Takao
+"""
+import numpy as np
+import networkx as nx
+from abc import ABCMeta, abstractmethod
+from typing import Dict, List, Union, Hashable
+from copy import deepcopy
+
+
+class Potential:
+    def __init__(self, tensor: np.ndarray, variables: Union[List, Hashable]):
+        """
+        This class represents potential functions, arising in the decomposition of joint distributions in Markov random fields.
+        (Can also be used to model conditional distributions in Bayesian networks)
+        We only consider the discrete variable setting here and not the continuous setting.
+        In this case, a potential function can be characterised by:
+        (1) a tensor, that stores the values of the potential function in tensorial form, and
+        (2) an ordered list of variable names, indicating which variable corresponds to which axis of the tensor.
+        
+        Example:
+        ----------
+        Consider a potential function in two variables a and b, with K discrete states X = {1, ..., K}.
+        Setting
+        - tensor = P, and
+        - variables = ['a', 'b']
+        for some K x K tensor P, yields a potential function f : X x X -> R, such that f(a=i, b=j) = P[i,j] for i,j = 1, ..., K.
+
+        Note: In general, when the potential is a function of N variables, each with K discrete states, then 'tensor' will be a K x ... x K (N times) tensor.
+        Note: If the tensor is one-dimensional, we accept any hashable type (e.g. int, str, etc...) for 'variables', indicating the label for the unique variable.
+        """
+        if isinstance(variables, Hashable):
+            variables = [variables]
+        assert tensor.ndim == len(variables), f"Dimensions of tensor ({tensor.ndim}) must match the number of variables {len(variables)}."
+        self.tensor = tensor
+        self.variables = variables
+        axes = np.arange(len(variables))
+        self.axes = dict(zip(variables, axes))
+
+
+class ProbabilisticGraphicalModel(nx.DiGraph, metaclass=ABCMeta):
+    """Base class for probabilistic graphical models"""
+    def __init__(self, *args, **kwargs):
+        self._num_states = None
+        self._variables = None
+
+        super().__init__(*args, **kwargs)
+
+        # TODO: Add node (state) and edge (message) attributes
+    
+    @property
+    def num_states(self):
+        if self._num_states is None:
+            raise NotImplementedError
+        else:
+            return self._num_states
+
+    @property
+    def variables(self):
+        if self._num_states is None:
+            raise NotImplementedError
+        else:
+            return list(set(self._variables)) # Remove duplicates
+
+    @property
+    def states(self):
+        return dict([(x, self.nodes[x]['state']) for x in self.variables])
+
+    @abstractmethod
+    def send_message(self, node, target_node, *args, **kwargs):
+        pass
+    
+    @abstractmethod
+    def update_state(self, node, *args, **kwargs):
+        pass
+
+    def send_all_messages(self, node):
+        for target_node in self.neighbors(node):
+            self.send_message(node, target_node)
+
+
+class TreeGraph(ProbabilisticGraphicalModel):
+    def __init__(self, edge_potentials: Union[Dict, List],
+                       node_potentials: Union[Dict, List]=None,
+                       state_type: str='marginal'):
+        super().__init__()
+
+        assert state_type in ['marginal', 'mode'], 'Only accept state_type = "marginal" or "mode"'
+        self.state_type = state_type
+
+        # Convert edge_potentials to dict if it is a list
+        if isinstance(edge_potentials, list):
+            edge_potentials_ = {}
+            for potential in edge_potentials:
+                variables = potential.variables
+                factorID = tuple(variables)
+                edge_potentials_[factorID] = potential
+        elif isinstance(edge_potentials, dict):
+            edge_potentials_ = edge_potentials
+        else:
+            raise TypeError
+
+        # Build graph from edge potentials
+        self._num_states = list(edge_potentials_.values())[0].tensor.shape[0]
+        self._variables = []
+        for factorID, potential in edge_potentials_.items():
+            (i,j) = potential.variables
+            self._variables.append(i)
+            self._variables.append(j)
+            self.add_node(i, state=self._initialise_state())
+            self.add_node(j, state=self._initialise_state())
+            self.add_edge(i, j, message=np.ones(self.num_states),
+                                ID=factorID,
+                                potential=potential)
+            self.add_edge(j, i, message=np.ones(self.num_states),
+                                ID=factorID,
+                                potential=potential)
+
+        # assert tree structure of graph
+        assert nx.is_tree(nx.Graph(self)), "The underlying graph must be a tree"
+
+        # Add node potentials to every node
+        node_potentials_ = dict([(node, Potential(np.ones(self.num_states), node)) for node in self.nodes()])
+        if node_potentials is None:
+            pass
+        elif isinstance(node_potentials, list):
+            for potential in node_potentials:
+                variables = potential.variables
+                node = variables[0]
+                node_potentials_[node] = potential
+        elif isinstance(node_potentials, dict):
+            for potential in node_potentials.values():
+                variables = potential.variables
+                node = variables[0]
+                node_potentials_[node] = potential
+        else:
+            raise TypeError
+
+        node_attrs = dict([(node, {'potential': potential}) for node, potential in node_potentials_.items()])
+        nx.set_node_attributes(self, node_attrs)
+
+    def _initialise_state(self):
+        if self.state_type == 'marginal':
+            return np.ones(self.num_states)
+        else:
+            return None
+
+    def send_message(self, source_node, target_node):
+        edge_potential = self.edges[(target_node, source_node)]['potential']
+        source_axis = edge_potential.axes[source_node] # Axis corresponding to the source node (0 or 1)
+        target_axis = edge_potential.axes[target_node] # Axis corresponding to the target node (0 or 1)
+        node_potential = self.nodes[source_node]['potential']
+        prod_msgs = deepcopy(node_potential.tensor)
+        for e in self.in_edges(source_node):
+            if e != (target_node, source_node):
+                prod_msgs *= self.edges[e]['message']
+        if self.state_type == 'marginal':
+            tensor = edge_potential.tensor
+            message = np.tensordot(tensor, prod_msgs, axes=(source_axis, 0))
+        elif self.state_type == 'mode':
+            tensor = edge_potential.tensor
+            prod_msgs = np.expand_dims(prod_msgs, axis=target_axis)
+            message = np.max(tensor * prod_msgs, axis=source_axis)
+        self.edges[(source_node, target_node)]['message'] = message
+
+    def update_state(self, node, parent_node=None, offsprings=None):
+        if self.state_type == 'marginal':
+            state = np.ones(self.num_states)
+            for e in self.in_edges(node):
+                state *= self.edges[e]['message']
+            state /= state.sum()
+            self.nodes[node]['state'] = state
+        elif self.state_type == 'mode':
+            x = self.nodes[parent_node]['state']
+            edge_potential = self.edges[(parent_node, node)]['potential']
+            node_potential = self.nodes[node]['potential']
+            prod_msgs = deepcopy(node_potential.tensor)
+            for child_node in offsprings:
+                prod_msgs *= self.edges[(child_node, node)]['message']
+            parent_axis = edge_potential.axes[parent_node] # Axis corresponding to the parent node (0 or 1)
+            if parent_axis == 0:
+                vector = edge_potential.tensor[x,:]
+            elif parent_axis == 1:
+                vector = edge_potential.tensor[:,x]
+            self.nodes[node]['state'] = np.argmax(vector * prod_msgs) # TODO: change
+
+
+class FactorGraph(ProbabilisticGraphicalModel):
+    def __init__(self, potentials: Union[Dict, List]):
+        """
+        Args:
+        ----------
+        :potentials: List of Potentials
+        """
+        super().__init__()
+
+        if isinstance(potentials, list):
+            potentials_ = {}
+            for potential in potentials:
+                variables = potential.variables
+                factorID = tuple(variables)
+                potentials_[factorID] = potential
+        elif isinstance(potentials, dict):
+            potentials_ = potentials
+        else:
+            raise TypeError
+
+        self._num_states = list(potentials_.values())[0].tensor.shape[0]
+        self._variables = []
+        self.factors = []
+        for factorID, potential in potentials_.items():
+            variables = potential.variables
+            for variableID in variables:
+                self._variables.append(variableID)
+                self.factors.append(factorID)
+                self.add_node(variableID, node_type='variable', state=np.ones(self.num_states))
+                self.add_node(factorID, node_type='factor', potential=potential)
+                self.add_edge(variableID, factorID, message=np.ones(self.num_states))
+                self.add_edge(factorID, variableID, message=np.ones(self.num_states))
+
+        assert nx.is_bipartite(self), "FactorGraph must be bipartite"
+
+    def send_message(self, source_node, target_node):
+        if self.nodes[source_node]['node_type'] == 'variable':
+            variabletofactormessage = np.ones(self.num_states)
+            for node in self.neighbors(source_node):
+                if (node != target_node):
+                    variabletofactormessage *= self.edges[(node, source_node)]['message']
+            variabletofactormessage /= variabletofactormessage.sum() # Normalising message lead to more stable results
+            self.edges[(source_node, target_node)]['message'] = variabletofactormessage 
+
+        elif self.nodes[source_node]['node_type'] == 'factor':
+            potential = self.nodes[source_node]['potential']
+            M = potential.tensor
+            for node in self.neighbors(source_node):
+                if node != target_node:
+                    variabletofactormessage = self.edges[(node, source_node)]['message']
+                    axis = potential.axes[node]
+                    M = np.tensordot(M, variabletofactormessage, axes=(axis, 0))
+                    M = np.expand_dims(M, axis=axis)
+            factortovariablemessage = M.squeeze()
+            factortovariablemessage /= factortovariablemessage.sum() # Normalising message yields more stable results
+            self.edges[(source_node, target_node)]['message'] = factortovariablemessage
+
+    def update_state(self, node):
+        assert self.nodes[node]['node_type'] == 'variable', 'State update can only be applied to variable nodes'
+        state = np.ones(self.num_states)
+        for edge in self.in_edges(node):
+            state *= self.edges[edge]['message'] # Multiply factor-to-variable message
+        state /= state.sum() # Normalise
+        self.nodes[node]['state'] = state
+
+
